0b26d6115356e6c3b004cff0d610224415372bf3
Viktor Abdukarimov <viredmi@gmail.com>
2022-09-25 15:01:48 +0300

    Added func for check redirecting

--- download_books.py
+++ download_books.py
@@ -1,7 +1,12 @@
 import os
 
 import requests
+from urllib.error import HTTPError
 
+
+def check_for_redirect(response: str) -> None:
+    if response.history:
+        raise requests.HTTPError
 
 def main() -> None:
     os.makedirs("books", exist_ok=True)
@@ -11,10 +16,13 @@ def main() -> None:
         url = f"https://tululu.org/txt.php?id={book_id}"
         response = requests.get(url)
         response.raise_for_status()
-        
-        filename = f"books/id{book_id}.txt"
-        with open(filename, "wb") as file:
-            file.write(response.content)
+        try: 
+            check_for_redirect(response)
+            filename = f"books/id{book_id}.txt"
+            with open(filename, "wb") as file:
+                file.write(response.content)
+        except:
+            continue
 
 if __name__ == "__main__":
     main()
