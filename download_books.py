cfef20bddb32c3d2e4cf130468386f9419f43b7c
Victor Abdukarimov <viredmi@gmail.com>
2022-09-26 19:34:23 +0300

    Download commits

--- download_books.py
+++ download_books.py
@@ -30,6 +30,19 @@ def download_image(url: str, folder: str = "images/") -> None:
         file.write(response.content)
 
 
+def download_comments(url: str, filename: str, folder: str = "comments/") -> list[str]:
+    os.makedirs(folder, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "lxml")
+    comments = soup.find("body").find("table").find_all(class_="texts")
+    filepath = os.path.join(folder, filename)
+    clear_filepath = sanitize_filepath(filepath)
+    with open(clear_filepath, "w") as file:
+        for comment in comments:
+            file.write(f"{comment.find('span').text}\n")
+    return comments
+
 def get_title_params_from_book(url: str) -> str:
     response = requests.get(url)
     response.raise_for_status()
@@ -50,20 +63,21 @@ def check_for_redirect(response: Response) -> None:
 
 def main() -> None:
     book_ids = list(range(1, 11))
-    image_url = get_title_params_from_book(f"https://tululu.org/b9/")[1]
-    download_image(image_url)
     for book_id in book_ids:
         url = f"https://tululu.org/txt.php?id={book_id}"
         try:
-            title = get_title_params_from_book(f"https://tululu.org/b{book_id}/")[0]
+            book_url = f"https://tululu.org/b{book_id}/"
+            title = get_title_params_from_book(book_url)[0]
             download_txt(url, f"{book_id}.{title}.txt")
-            image_url = get_title_params_from_book(f"https://tululu.org/b{book_id}/")[1]
+            image_url = get_title_params_from_book(book_url)[1]
             download_image(image_url)
             print(title)
             print(image_url)
+            comments = download_comments(book_url, f"{book_id}_comments.txt")
+            for comment in comments:
+                print(comment.find("span").text)
         except:
             continue
-
 if __name__ == "__main__":
     main()
 
