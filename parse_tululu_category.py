1132355e9e55e50b648ac461287d041f1240e79b
Victor Abdukarimov <viredmi@gmail.com>
2022-10-03 13:50:07 +0300

    Downloaded books from all pages

---
+++ parse_tululu_category.py
@@ -0,0 +1,57 @@
+import time
+import json
+from urllib.parse import urljoin, urlsplit
+
+import requests
+from bs4 import BeautifulSoup
+from loguru import logger
+
+from download_books import download_txt, download_image, parse_book_page, \
+    check_for_redirect
+
+
+def main() -> None:
+    page_ids = range(5)
+    books_description = []
+    for page_id in page_ids:
+        url = f"https://tululu.org/l55/{page_id}/"
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "lxml")
+        books_on_page = soup.find("body").find(class_="tabs") \
+            .find_all(class_="bookimage")
+        for book in books_on_page:
+            try:
+                url = f"https://tululu.org/txt.php"
+                book_url = urljoin("https://tululu.org", book.find("a")["href"])
+                book_id = urlsplit(book_url).path.strip("/b")
+                payload = {"id": f"{book_id}"}    
+                response = requests.get(book_url)
+                response.raise_for_status()
+                check_for_redirect(response)
+                book_page = response.text
+                parsed_page = parse_book_page(book_page)
+                book_description = {
+                    "title": parsed_page.title,
+                    "author": parsed_page.author,
+                    "img_src": parsed_page.image,
+                    "book_path": f"books/{parsed_page.title}.txt",
+                    "comments": parsed_page.comments,
+                    "genres": parsed_page.genres
+                }
+                books_description.append(book_description)
+                download_image(parsed_page.image)
+                download_txt(url, payload, f"{parsed_page.title}.txt")
+            except requests.HTTPError:
+                logger.exception("Книги с таким id нет!")
+                continue
+            except requests.ConnectionError:
+                logger.exception("Нет соединения!")
+                time.sleep(15)
+    json_books_description = json.dumps(books_description, ensure_ascii=False)
+    with open("books_info.json", "w") as file:
+        file.write(json_books_description)
+
+
+if __name__ == "__main__":
+    main()
