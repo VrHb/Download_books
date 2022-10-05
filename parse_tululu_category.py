bebe2d5156cfcad3b7d2b529660630c163341445
Victor Abdukarimov <viredmi@gmail.com>
2022-10-06 00:17:36 +0300

    Split code by into functions

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,18 +1,34 @@
 import argparse
 import os
 import time
+from typing import NamedTuple
 import json
 from urllib.parse import urljoin, urlsplit
+from bs4.element import ResultSet
 
 import requests
 from bs4 import BeautifulSoup
 from loguru import logger
 
-from download_books import download_txt, download_image, parse_book_page, \
+from download_books import ParsedPage, download_txt, download_image, parse_book_page, \
     check_for_redirect
 
 
-def main() -> None:
+class Argument(NamedTuple):
+    start_page: int
+    end_page: int
+    skip_img: str
+    skip_txt: str
+    dest_folder: str
+    json_path: str
+
+class Book(NamedTuple):
+    book_id: str
+    description: dict
+    parsed: ParsedPage
+
+
+def get_arguments() -> Argument:
     parser = argparse.ArgumentParser(
         description="Постранично загружаем книги с сайта tululu.org"
     )
@@ -47,51 +63,72 @@ def main() -> None:
         help="Путь для сохранения json с информацией по книгам"
     )
     args = parser.parse_args()
+    return Argument(
+        start_page=args.start_page,
+        end_page=args.end_page,
+        skip_img=args.skip_img,
+        skip_txt=args.skip_txt,
+        dest_folder=args.dest_folder,
+        json_path=args.json_path
+    )
 
-    page_ids = range(int(args.start_page), int(args.end_page))
+
+def get_books_on_page(url: str) -> ResultSet:
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "lxml")
+    selector = "body table.tabs div.bookimage"
+    books_on_page = soup.select(selector)
+    return books_on_page
+
+
+def get_book_description(book: str, arguments: Argument) -> Book:
+    book_url = urljoin("https://tululu.org", book.select_one("a")["href"])
+    book_id = urlsplit(book_url).path.strip("/b")
+    response = requests.get(book_url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    book_page = response.text
+    parsed_page = parse_book_page(book_page)
+    book_description = {
+        "title": parsed_page.title,
+        "author": parsed_page.author,
+        "img_src": parsed_page.image,
+        "book_path": f"{arguments.dest_folder}/books/{parsed_page.title}.txt",
+        "comments": parsed_page.comments,
+        "genres": parsed_page.genres
+    }
+    return Book(book_id, book_description, parsed_page)
+
+
+def main() -> None:
+    arguments = get_arguments()
+    page_ids = range(int(arguments.start_page), int(arguments.end_page))
     books_description = []
     for page_id in page_ids:
         url = f"https://tululu.org/l55/{page_id}/"
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, "lxml")
-        selector = "body table.tabs div.bookimage"
-        books_on_page = soup.select(selector)
+        books_on_page = get_books_on_page(url)
         for book in books_on_page:
             try:
+                book = get_book_description(book, arguments)
                 url = f"https://tululu.org/txt.php"
-                book_url = urljoin("https://tululu.org", book.select_one("a")["href"])
-                book_id = urlsplit(book_url).path.strip("/b")
-                payload = {"id": f"{book_id}"}    
-                response = requests.get(book_url)
-                response.raise_for_status()
-                check_for_redirect(response)
-                book_page = response.text
-                parsed_page = parse_book_page(book_page)
-                book_description = {
-                    "title": parsed_page.title,
-                    "author": parsed_page.author,
-                    "img_src": parsed_page.image,
-                    "book_path": f"{args.dest_folder}/books/{parsed_page.title}.txt",
-                    "comments": parsed_page.comments,
-                    "genres": parsed_page.genres
-                }
-                books_description.append(book_description)
-                os.makedirs(args.dest_folder, exist_ok=True)
-                if args.skip_img:
+                payload = {"id": f"{book.book_id}"}    
+                books_description.append(book.description)
+                os.makedirs(arguments.dest_folder, exist_ok=True)
+                if arguments.skip_img:
                     download_image(
-                        parsed_page.image,
-                        os.path.join(args.dest_folder, "images")
+                        book.parsed.image,
+                        os.path.join(arguments.dest_folder, "images")
                     )
-                if args.skip_txt:
+                if arguments.skip_txt:
                     download_txt(
                         url,
                         payload,
-                        f"{parsed_page.title}.txt",
-                        os.path.join(args.dest_folder, "books") 
+                        f"{book.parsed.title}.txt",
+                        os.path.join(arguments.dest_folder, "books") 
                     )
-                logger.info(f"Название книги: {parsed_page.title}")
-                logger.info(f"Автор: {parsed_page.author}")
+                logger.info(f"Название книги: {book.parsed.title}")
+                logger.info(f"Автор: {book.parsed.author}")
             except requests.HTTPError:
                 logger.exception("Книги с таким id нет!")
                 continue
@@ -104,8 +141,8 @@ def main() -> None:
         indent=4,
         ensure_ascii=False
     )
-    os.makedirs(args.json_path, exist_ok=True)
-    with open(f"{args.json_path}/books_info.json", "w") as file:
+    os.makedirs(arguments.json_path, exist_ok=True)
+    with open(f"{arguments.json_path}/books_info.json", "w") as file:
         file.write(json_books_description)
 
 
